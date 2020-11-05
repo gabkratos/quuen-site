89035ea2e50a8b491b97aaa1488f9e325ef6979e
gabkratos <61694082+gabkratos@users.noreply.github.com>
2020-11-05 19:42:04 -0300

    Delete script2.js

--- css/script2.js
+++
@@ -1,9 +0,0 @@
-let txt1 = document.getElementById("email");
-let txt2 = document.getElementById("Senha");
-
-function change(){
-    const infos = [0,0];
-    infos[0] = txt1.value;
-    infos[1] = txt2.value;
-       console.log(infos);
-}
