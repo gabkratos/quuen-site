9bee942874fbd47fe190ef0cdf5bf55912a15eb7
gabkratos <61694082+gabkratos@users.noreply.github.com>
2020-11-05 19:15:56 -0300

    Add files via upload

---
+++ css/script2.js
@@ -0,0 +1,9 @@
+let txt1 = document.getElementById("email");
+let txt2 = document.getElementById("Senha");
+
+function change(){
+    const infos = [0,0];
+    infos[0] = txt1.value;
+    infos[1] = txt2.value;
+       console.log(infos);
+}
